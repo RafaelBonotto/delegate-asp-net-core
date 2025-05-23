3034f1f063cf303bfba4fa6db590a0c41d3652d7
Rafael Bonotto <rafareisaba@outlook.com>
2025-05-22 22:54:54 -0300

    Adiciona Action Delegate

---
+++ Delegates/Delegates/TiposDelegates/ActionDelegate.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegates.TiposDelegates
+{
+    public class ActionDelegate
+    {
+        Action<string> ImprimirMensagem = mensagem => Console.WriteLine(mensagem);
+        Action<int, string> AlterarNomeAction = (id, nome) => AlterarNome(id, nome);
+
+        public static void AlterarNome(int id, string nome) => Console.WriteLine($"Nome alterado - Id: {id}, Nome: {nome}");
+    }
+}
