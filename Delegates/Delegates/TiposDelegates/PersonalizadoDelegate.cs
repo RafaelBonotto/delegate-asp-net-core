1dd70c6f531f24ea98ac4f33c52e23d22e54b922
Rafael Bonotto <rafareisaba@outlook.com>
2025-05-24 19:39:04 -0300

    Adiciona Personal Delegate

---
+++ Delegates/Delegates/TiposDelegates/PersonalizadoDelegate.cs
@@ -0,0 +1,16 @@
+﻿namespace Delegates.TiposDelegates
+{
+    public class PersonalizadoDelegate
+    {
+        public delegate int Operacao(int x, int y);
+
+        public static void TestarDelegatePersonalizado()
+        {
+            Operacao Somar = (a, b) => a + b;
+            Operacao Multiplicar = (a, b) => a * b;
+
+            Console.WriteLine($"Delegate Personalizado Somar 3 + 5: {Somar(3, 5)}");       // Saída: 8
+            Console.WriteLine($"Delegate Personalizado Multiplicar 3 x 5: {Multiplicar(3, 5)}");       // Saída: 15
+        }
+    }
+}
