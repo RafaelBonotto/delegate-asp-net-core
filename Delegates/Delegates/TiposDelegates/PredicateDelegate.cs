399fd0f35020d7d3961a37069da9ded31bb155d6
Rafael Bonotto <rafareisaba@outlook.com>
2025-05-23 22:17:44 -0300

    Adiciona Delegate Predicate

---
+++ Delegates/Delegates/TiposDelegates/PredicateDelegate.cs
@@ -0,0 +1,8 @@
+﻿namespace Delegates.TiposDelegates
+{
+    public class PredicateDelegate
+    {
+        // Sempre irá retornar um booleano
+        Predicate<int> ValidarNumeroPar = numero => numero % 2 == 0;
+    }
+}
