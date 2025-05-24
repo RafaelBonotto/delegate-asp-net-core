b5c70cb43ec1877f399613b129bb43d11b19d9d9
Rafael Bonotto <rafareisaba@outlook.com>
2025-05-24 00:10:17 -0300

    Adiciona Func Delegate

---
+++ Delegates/Delegates/TiposDelegates/FuncDelegate.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegates.TiposDelegates
+{
+    public class FuncDelegate
+    {
+        Func<int, int, int> Multiplicar = (x, y) => x * y;
+        Func<double, double, double> Dividir = (a, b) => a / b;
+        Func<string, int> ContarCaracteres = s => s.Length;
+        Func<string> Saudacao = () => "Hello World!";
+    }
+}
